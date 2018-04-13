bf91f780614733967c7711d1979c3e905340ee2f
Marcus Jun <mjun@calstate.edu>
2018-04-13 01:15:12 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,6 +16,12 @@ class String
 
   def count_sentences
     count=0
+    #I kind-of cheated. Instead of splitting the sentence by .?!
+    #I split the sentence by whitespace then counted the number of
+    #"words" ending in . or ? or !
+    #If you followed the lab instructions, you'd end up with an
+    #array of "sentences" without ending punctuation so you
+    #couldn't even use the three methods created above.
     pieces=self.split#(" ")
     pieces.each do |word|
       if word.sentence? || word.question? || word.exclamation?
