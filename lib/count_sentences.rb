2cf0f8392240452f7544602bd2a588a8c90d23a4
Marcus Jun <mjun@calstate.edu>
2018-04-13 01:35:32 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,19 +15,32 @@ class String
   end
 
   def count_sentences
-    count=0
-    #I kind-of cheated. Instead of splitting the sentence by .?!
-    #I split the sentence by whitespace then counted the number of
-    #"words" ending in . or ? or !
-    #If you followed the lab instructions, you'd end up with an
-    #array of "sentences" without ending punctuation so you
+
+    #Solution based on lab Instructions
+    #Unfortunately, you end up with an array of "sentences"
+    #without ending punctuation so you
     #couldn't even use the three methods created above.
-    pieces=self.split#(" ")
-    pieces.each do |word|
-      if word.sentence? || word.question? || word.exclamation?
-        count+=1
-      end
-    end
+    #Then why would you even have created those three methods?
+
+    #pieces=self.split(/[\.\?\!]/)
+    #pieces.delete_if {|word| word==""}
+    #pieces.count
+
+    #############################################################
+
+    #My way: Instead of splitting the sentence by .?!
+    #I split the string by whitespace then counted the number of
+    #"words" ending in . or ? or !
+
+    count=0
+    self.split.each {|word| count+=1 if word.sentence? || word.question? || word.exclamation?}
     count
+
+    #pieces=self.split#(" ")
+    #pieces.each {|word| count+=1 if word.sentence? || word.question? || word.exclamation?}
+    #pieces.each do |word|
+        #count+=1 if word.sentence? || word.question? || word.exclamation?
+    #end
+
   end
 end
